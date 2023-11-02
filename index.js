13d74e5fd65b011f23678fa0ed169013fa1f72c4
Fardin7864 <fardinhasan1888@gmail.com>
2023-11-02 22:22:01 +0600

    create api for jwt

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
+var cookieParser = require('cookie-parser')
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7k1zdza.mongodb.net/?retryWrites=true&w=majority`;
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors(
+    {
+        origin: ['http://localhost:5173'],
+        credentials: true,
+        methods: ["GET", "POST", "DELETE", "UPDATE"]
+    }
+));
+
+//custom middlewares
+// const veify = 
 
 app.get("/", async (req, res) => {
   res.send("Ema-zohan Server is runnig!");
@@ -26,11 +38,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //collection
     const productsColl = client.db("ema-zohan").collection("products");
     const cartColl = client.db("ema-zohan").collection("cart");
+
+    //Auth related api
+    app.post('/jwt',async (req,res) => {
+        const email = req?.body;
+        const token =  jwt.sign(email, process.env.API_SECRET_KEY,{expiresIn: '1h'})
+        try {
+            console.log(token);
+            res
+            .cookie('ema-zohan', token,{httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'})
+            .send(token)
+        } catch (error) {
+            
+        }
+     })
 
     //Get all products
     app.get("/products", async (req, res) => {
